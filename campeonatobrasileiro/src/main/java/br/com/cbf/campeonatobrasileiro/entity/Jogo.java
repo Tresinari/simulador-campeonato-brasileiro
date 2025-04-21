aec82f3747a10430f33a10adcaea72b57b7346b9
Tresinari <leotaguiar0@gmail.com>
2025-04-21 17:19:21 -0300

    Configuração inicial das entidades Time e Jogo

---
+++ campeonatobrasileiro/src/main/java/br/com/cbf/campeonatobrasileiro/entity/Jogo.java
@@ -0,0 +1,21 @@
+package br.com.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//@Data
+//@Entity
+public class Jogo {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//    private Time time1;
+//    private Time time2;
+//    private Integer golsTime1;
+//    private Integer golsTime2;
+//    private Integer publicoPagante;
+
+}
---
+++ campeonatobrasileiro/src/main/java/br/com/cbf/campeonatobrasileiro/entity/Time.java
@@ -0,0 +1,19 @@
+package br.com.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 20)
+    private String nome;
+    @Column(length = 4)
+    private String sigla;
+    @Column(length = 2, name = "estado")
+    private String uf;
+
+}
